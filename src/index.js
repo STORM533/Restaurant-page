2271d0ef26a37c5c7bf3cc52e68f732e84765ae9
STORM533 <146557098+STORM533@users.noreply.github.com>
2025-08-27 20:57:13 +0530

    added new fonts and complete front page

--- src/index.js
+++ src/index.js
@@ -1,8 +1,31 @@
 import "./styles.css";
-console.log("hello");
 import cafeImage from "./assets/images/cafeImage.jpg"
+const content = document.querySelector("#content");
+const texts = document.createElement("div");
+texts.classList.add("texts");
+const intro1 = document.createElement("p");
+const intro2 = document.createElement("p");
+const intro3 = document.createElement("p");
+const btn = document.createElement("button");
+btn.classList.add("order");
+btn.textContent = "ORDER";
+intro1.classList.add("intro1");
+intro2.classList.add("intro2");
+intro3.classList.add("intro3"); 
+texts.appendChild(intro1);
+texts.appendChild(intro2);
+texts.appendChild(intro3);
+texts.appendChild(btn);
+intro1.textContent = "Welcome onboard";
+intro2.textContent = "STORM KITCHEN";
+intro3.textContent = "Enjoy exquisite delicacies and drinks!!!";
 const image  = document.createElement("img");
 image.src = cafeImage;
-document.body.appendChild(image);
-image.style.width = "200px";
-image.style.height = "200px";
+content.appendChild(image);
+content.appendChild(texts);
+image.style.width = "800px";
+image.style.height = "500px";
+image.style.borderRadius = "20px";
+image.style.boxShadow = "0 10px 10px rgba(0, 0, 0, 0.5)";
+
+
