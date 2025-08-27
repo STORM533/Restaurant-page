1e6c662cfb60105799a0c6211d3d2239972545d0
STORM533 <146557098+STORM533@users.noreply.github.com>
2025-08-27 19:24:09 +0530

    added assets folder containing images and fonts

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
+import "./styles.css";
 console.log("hello");
+import cafeImage from "./assets/images/cafeImage.jpg"
+const image  = document.createElement("img");
+image.src = cafeImage;
+document.body.appendChild(image);
+image.style.width = "200px";
+image.style.height = "200px";
